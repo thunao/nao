333b6392339ff0b9d29506739efe1f5ba98cfc4b
Dstray <lixt12@qq.com>
2015-07-17 22:08:47 +0800

    Release the dependency on cv2.cv and other updates

--- legacy/VideoDevice.py
+++ legacy/VideoDevice.py
@@ -14,18 +14,24 @@ def str2array(string, shape):
 				image[i, j, c] = ord(string[p1 + p2 + p3])
 	return image
 
+def isChessboardFound(image, board_sz):
+	found, corners = cv2.findChessboardCorners(
+		image, board_sz, None,
+		cv2.CALIB_CB_ADAPTIVE_THRESH | cv2.CALIB_CB_FILTER_QUADS)
+	return found
+
 # init camera
 local = "127.0.0.1"
-remote = "101.5.217.2"
-vd = ALProxy("ALVideoDevice", local, 9559)
+remote = "101.5.222.14"
+vd = ALProxy("ALVideoDevice", local, 62501)
 nameID = "VM"
 cameraIdx = 0
-resolution = 1 # kQQQQVGA(40*30) = 8 | kVGA(640*480) = 2
+resolution = 2 # kQQQQVGA(40*30) = 8 | kVGA(640*480) = 2
 colorSpace = 11 # kRGBColorSpace (vision_definitions)
-fps = 15
+fps = 10
 nameID = vd.subscribeCamera(nameID, cameraIdx, resolution, colorSpace, fps)
-print nameID
-print vd.getResolution(nameID)
+print 'NAME ID:', nameID
+print 'Resolution:', vd.getResolution(nameID)
 if not vd.isCameraOpen(cameraIdx):
 	vd.openCamera(cameraIdx)
 
@@ -35,7 +41,9 @@ cv2.namedWindow(windowName, 1)
 delay = 1000 / fps
 
 # capture images
-while cv2.waitKey(delay) < 0:
+count = 1
+key = cv2.waitKey(delay)
+while not key == 27:
 	imgVal = vd.getImageRemote(nameID)
 	width = imgVal[0]
 	height = imgVal[1]
@@ -43,6 +51,11 @@ while cv2.waitKey(delay) < 0:
 	shape = (height, width, nchanels)
 	image = str2array(imgVal[6], shape)
 	cv2.imshow(windowName, image)
+	if key == 13 or isChessboardFound(image, (5, 5)):
+		cv2.imwrite('cb' + str(count) + '.jpg', image)
+		print 'Picture', count
+		count += 1
+	key = cv2.waitKey(delay)
 
 vd.unsubscribe(nameID)
 vd.setAllParametersToDefault(cameraIdx)
