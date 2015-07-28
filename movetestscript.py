3a00f77ad54b0e863eb9f716e63530909f287b07
陈天昱 <chentianyu@outlook.com>
2015-07-28 15:57:07 +0800

    fix indent

--- movetestscript.py
+++ movetestscript.py
@@ -2,15 +2,14 @@ from posture import Motion
 from math import pi
 
 def test(IP='127.0.0.1', PORT=9559):
-
 	# init a Motion instance
 	motion = Motion(IP, PORT)
 
 	motion.walkStraight((5, 0))
 	motion.turn(pi / 2)
-    motion.walkStraight((5, 8))
-    motion.turn(- pi / 2)
-    motion.walkStraight((10, 8))
+	motion.walkStraight((5, 8))
+	motion.turn(- pi / 2)
+	motion.walkStraight((10, 8))
 
 	del motion
 
