2d4d1804224cfce918f695248c3eee2f2f4f3fe1
X-Buring <wanghao1993333@gmail.com>
2015-08-04 11:35:54 +0800

    test(dying)

--- nao.py
+++ nao.py
@@ -5,12 +5,13 @@ from movedetermine import MoveDetermine
 from posture import Motion, HeadLoc
 from ball import Ball
 from path.astar import astar
-from stereo_calibration.detect_obstacle import detect_obstacle
+from stereo_calibration.detect_obstacle import detect_obstacle, display2D
 
 import sys
 import getopt
 from math import sin, cos, pi
 import pickle
+import json
 
 def __isnear(p1, p2, t = 5):
 	if abs(p1[0]-p2[0]) + abs(p1[1]-p2[1]) > t:
@@ -20,9 +21,13 @@ def __isnear(p1, p2, t = 5):
 #现在只是伪代码，基本不能跑
 
 ip = "127.0.0.1"
-if len(sys.argv) == 2:
+test = False
+if len(sys.argv) >= 2:
 	ip = sys.argv[1]
-threshold = 10
+if len(sys.argv) >= 3:
+	test = True
+wallthreshold = 500
+floorthreshold = 50
 headheight = 44.7
 topcarmeraheight = 6.364
 topcarmeadown = 1.2 /180 * pi
@@ -34,17 +39,22 @@ target = (0, 0)
 while True:
 	vote = {}
 	points = list()
-	pics = motion.lookAround()
+	if test:
+		with open('pics.txt', "r") as ff:
+			pics = pickle.load(ff)
+	else:
+		pics = motion.lookAround()
 	(x, y, toward) = motion.getPosition()
 	(realx, realy) = movedetermine.mymap.maptorobot(x, y)
 	myposition = (x, y)
 	movedetermine.mymap.moveto(x, y)
 	movedetermine.mymap.turnto(sin(toward), cos(toward))
 
-	with open('pics.txt', "w") as f:
-		pickle.dump(pics, f)
+	if not test:
+		with open('pics.txt', "w") as f:
+			pickle.dump(pics, f)
 
-	print "照完相，开始看图"
+	print "taken pics"
 	#points = kan.shibie(position, pics)
 	for i in pics:
 		p = pics[i].keys()
@@ -52,21 +62,23 @@ while True:
 		print p[0].left, ' ', p[0].down
 		#find = Ball(pics[i][j].tolist())
 		l = detect_obstacle(pics[i][p[0]], pics[i][p[1]], (realx, realy, headheight + topcarmeraheight), \
-							(i + p[0].left, topcarmeadown + p[0].down), -1) 
+							(i + p[0].left, topcarmeadown + p[0].down), -100) 
+		display2D(l)
 		points.extend(l)
 	print "size of pointlist is ", len(points)
 
-	with open('points.txt', "w") as ff:
-		pickle.dump(points, ff)
+	#if not test:
+	#	with open('points.txt', "w") as ff:
+	#		pickle.dump(points, ff)
 
-	print "看完图了，开始标点"
+	print "add point"
 	jishuqi = 0
 	for i in points:
 		p = movedetermine.mymap.robottomap(i[0], i[1])
 		tmp = [0, 0]
 		if p in vote:
 			tmp = vote[p]
-		if i[2] > 5:
+		if i[2] > 50:
 			tmp[1] += 1
 		else:
 			tmp[0] += 1
@@ -75,21 +87,30 @@ while True:
 		if jishuqi % 10000 == 0:
 			print jishuqi
 
-	with open("vote.txt", "w") as fff:
-		pickle.dump(vote, fff)
+	#with open("vote.txt", "w") as fff:
+	#	json.dump(vote, fff)
 
-	print "标完点了，决定障碍物"
+
+	print "vote wall"
+	with open("vote.txt", "w") as fff:
+		for i in range(-100, 100):
+			for j in range(-100, 100):
+				if (i, j) in vote:
+					fff.write("(%d, %d) [%d, %d]\n" % (i, j, vote[(i, j)][0], vote[(i, j)][1])) 
 	for i in vote:
 		tmp = vote[i]
-		if tmp[1] > threshold:
+		if tmp[1] > wallthreshold:
 			movedetermine.mymap.addpoint(i[0], i[1], 2)
-		elif tmp[0] > threshold:
+		elif tmp[0] > floorthreshold:
 			movedetermine.mymap.addpoint(i[0], i[1], 1)
 
-	with open("mymap.txt", "w") as ffff:
-		pickle.dump(movedetermine.mymap, ffff)
+	movedetermine.mymap.printmap()
+	#print movedetermine.mymap.Map
+	#with open("mymap.txt", "w") as ffff:
+	#	json.dump(movedetermine.mymap.Map, ffff)
 
 	if __isnear(target, myposition):
+		print "near!"
 		target = move.pointtogo()
 	pathfind = astar(movedetermine.mymap, myposition, target)
 	nextstep = pathfind.get_result()
