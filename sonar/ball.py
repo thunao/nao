230755f36f12811dfc08d31f8fc0dd6f9ca735a2
badpoem <liruoyusince1995@gmail.com>
2015-08-09 20:52:33 +0800

    Update ball.py

--- sonar/ball.py
+++ sonar/ball.py
@@ -1,7 +1,7 @@
 from PIL import Image,ImageFilter
 import cv2
 import numpy as np
-from gaussianblur import MyGaussianBlur
+from gaussianblur import MyGaussianBlur # 另一种高斯平滑方式，采用PIL库
 from time import time
 
 class Ball():
@@ -12,13 +12,13 @@ class Ball():
         matrix = [[0 for col in range(width)] for row in range(height)]
         imgX = [[[0] for col in range(width)] for row in range(height)]
         tv = 0
-        hsv = cv2.cvtColor(list1, cv2.COLOR_BGR2HSV)
+        hsv = cv2.cvtColor(list1, cv2.COLOR_BGR2HSV) # 将读入图片由bgr颜色空间转换到hsv
         hsv = hsv.tolist()
         list1 = list1.tolist()
         for hei in range(0, height):
             for w in range(0, width):
-                tv += hsv[hei][w][2] / 255.
-        if tv > 210000:
+                tv += hsv[hei][w][2] / 255. # 求取图片像素点v值之和
+        if tv > 210000: # 借由v值之和对于光暗条件加以判断
             threshole = 0.9
         elif tv > 150000:
             threshole = 0.75
@@ -37,7 +37,7 @@ class Ball():
                 h *= 2
                 s /= 255.
                 v /= 255.
-                if ((h < 30 and h > 0) or (h > 355) or (h == 0 and r > g)) and s > 0.1:
+                if ((h < 30 and h > 0) or (h > 355) or (h == 0 and r > g)) and s > 0.1: # 对橙色色彩加以识别
                     if v > threshole:
                         matrix[hei][w] = 1
         for hei in range(1, height-1):
@@ -47,9 +47,9 @@ class Ball():
                     count = (matrix[hei-1][w-1] + matrix[hei-1][w] + matrix[hei-1][w+1]
                         + matrix[hei][w-1] + matrix[hei][w+1]
                         + matrix[hei+1][w-1] + matrix[hei+1][w] + matrix[hei+1][w+1])
-                imgX[hei][w][0] = 255 if count >= 5 else 0
-        f = cv2.GaussianBlur(np.array(imgX, np.uint8), (11, 11), 5)
-        circles = cv2.HoughCircles(f,cv2.HOUGH_GRADIENT,1,5,param1=100,param2=30,minRadius=10,maxRadius=150)
+                imgX[hei][w][0] = 255 if count >= 5 else 0 # 判断置1点周围是否有5个以上置1点，并重绘
+        f = cv2.GaussianBlur(np.array(imgX, np.uint8), (11, 11), 5) # 高斯平滑
+        circles = cv2.HoughCircles(f,cv2.HOUGH_GRADIENT,1,5,param1=100,param2=30,minRadius=10,maxRadius=150) # 霍夫圆检测
         count = 0
         self.loc1 = 0
         self.loc2 = 0
@@ -61,16 +61,16 @@ class Ball():
                 self.loc1 += i[0]
                 self.loc2 += i[1]
                 self.ra += i[2]
-            self.loc1 = int(self.loc1/count)
-            self.loc2 = int(self.loc2/count)
-            self.ra = int(self.ra/count)
+            self.loc1 = int(self.loc1/count) # 圆心x坐标
+            self.loc2 = int(self.loc2/count) # 圆心y坐标
+            self.ra = int(self.ra/count) # 半径
             #cv2.circle(result,(self.loc1,self.loc2),self.ra,(0,255,0),1)
             #cv2.circle(result,(self.loc1,self.loc2),2,(0,0,255),3)
 
     def getLoc(self):
-        return (self.loc1, self.loc2)
+        return (self.loc1, self.loc2) # 返回圆心坐标
 
-    def getR(self):
+    def getR(self): # 返回半径
         return self.ra
 
     def max3(self, r, g, b):
@@ -94,4 +94,3 @@ if __name__ == "__main__":
     ball = Ball(list1)
     print ball.getLoc()
     print ball.getR()
-
